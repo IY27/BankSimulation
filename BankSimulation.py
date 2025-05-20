3f4ccd73b8963cbd383e01b50ca5c0d8f3f0dff5
IY27 <ivanyang27@gmail.com>
2025-05-20 15:05:28 +1200

    Added foundation of createAccount function

--- BankSimulation.py
+++ BankSimulation.py
@@ -10,24 +10,49 @@ def askAccount():
         if askaccount == "yes":
             login()
         elif askaccount == "no":
-            makeaccount = input("Would you like to create an account? : (Yes/No) ").lower().strip()
-            if makeaccount == "yes":
-                createAccount()
-            elif makeaccount == "no":
-                exit_option = input("Would you like to exit? (Yes/No) ").lower().strip()
-                if exit_option == "yes":
-                    quit()
-                elif exit_option == "no":
-                    continue
-                else:
-                    print("Please enter Yes or No")
-            else:
-                print("Please enter Yes or No")
-        else: 
+            createAccount()
+        else:
             print("Please enter Yes or No")
+            askaccount = input("Do you have a bank account? : (Yes/No) ").lower().strip()
+
+MIN_AGE = 13
+MAX_AGE = 18
 
 def createAccount():
-    None
+    makeaccount = input("Would you like to create an account? : (Yes/No) ").lower().strip()
+    if makeaccount == "no":
+        exit()
+    elif makeaccount == "yes":
+        try:
+            age = int(input("Enter your age : "))
+            while age < MIN_AGE:
+                print("You cannot be younger than 13 years old")
+                age = int(input("Enter your age : "))
+            while age > MAX_AGE:
+                print("You cannot be older than 18 years old")
+                age = int(input("Enter your age : "))
+            else:
+                print("You are eligible to create an account")
+            username = input("Enter Username : ")
+            while " " in username:
+                print("There must not be spaces within the username.")
+                username = input("Enter Username : ")
+            while username == "":
+                print("Please enter an username")
+                username = input("Enter Username : ")
+            else:
+                password = input("Enter Password : ").strip()
+                while password == "":
+                    print("Please enter a password")
+                    password = input("Enter Password : ").strip()
+                else:
+                    login()
+                    return
+        except ValueError:
+            print("Please enter an integer")
+    else:
+        print("Please enter Yes or No")
+        createAccount()
 
 def login():
     None
@@ -51,7 +76,13 @@ def showBalance():
     None
 
 def exit():
-    None
-
+    while True:
+        exit_option = input("Would you like to exit? (Yes/No) ").lower().strip()
+        if exit_option == "yes":
+            quit()
+        elif exit_option == "no":
+            return
+        else: 
+            print("Please enter Yes or No")
 description()
 askAccount()
